0e2420a0ae98081f1644aaa987fcc6f7521ceaba
Pratteek Shaurya <pratteek.shaurya1994@gmail.com>
2023-08-10 23:33:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigator from './navigator/MainNav';
 
-function App(): JSX.Element {
+const App = () => {
   return (
     <NavigationContainer>
       <MainNavigator />
     </NavigationContainer>
   );
-}
+};
 
 export default App;
